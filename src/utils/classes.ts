1c8fbeae2b3b1530227184dee788ce1f64aa5b9e
Vanaldito <barraza.fredimanuel@gmail.com>
2023-11-13 11:52:23 -0600

    feat: add classes utility function to src/utils

---
+++ src/utils/classes.ts
@@ -0,0 +1,16 @@
+type ClassToggles = { [key: string]: boolean };
+
+export default function classes(...data: (string | ClassToggles)[]) {
+  return data
+    .map(item => {
+      if (typeof item === "string") {
+        return item;
+      }
+
+      return Object.entries(item)
+        .filter(item => item[1])
+        .map(item => item[0])
+        .join(" ");
+    })
+    .join(" ");
+}
