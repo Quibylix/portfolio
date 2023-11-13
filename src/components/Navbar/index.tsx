1003ab9dffcd4ceca119d581ff05f057754878b2
Vanaldito <barraza.fredimanuel@gmail.com>
2023-11-13 13:46:03 -0600

    feat: add dropdown menu to Navbar component

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,15 +1,49 @@
+import { useState } from "react";
+import { classes } from "../../utils";
 import Icon from "../Icon";
 import styles from "./Navbar.module.css";
 
 export default function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  function toggleMenu() {
+    setIsMenuOpen(!isMenuOpen);
+  }
+
   return (
     <nav className={styles.navbar}>
       <a href="#" className={styles.logo}>
         <Icon icon="logo" />
       </a>
-      <button className={styles.menu}>
+      <button onClick={toggleMenu} className={styles.menu}>
         <Icon icon="menu" />
       </button>
+      <ul
+        className={classes(styles.dropdown, {
+          [styles.dropdownOpen]: isMenuOpen,
+        })}
+      >
+        <li>
+          <a className={styles.link} href="#">
+            Home
+          </a>
+        </li>
+        <li>
+          <a className={styles.link} href="#about-me">
+            About me
+          </a>
+        </li>
+        <li>
+          <a className={styles.link} href="#my-projects">
+            My projects
+          </a>
+        </li>
+        <li>
+          <a className={styles.link} href="#contact-me">
+            Contact me
+          </a>
+        </li>
+      </ul>
     </nav>
   );
 }
