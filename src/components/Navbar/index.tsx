c3b1e556ef6416af328db8ed9f5d353a88c04925
Vanaldito <barraza.fredimanuel@gmail.com>
2023-11-13 14:09:43 -0600

    feat: add menu auto-close functionality to Navbar component

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,21 +1,41 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { classes } from "../../utils";
 import Icon from "../Icon";
 import styles from "./Navbar.module.css";
 
 export default function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const menuButton = useRef<HTMLButtonElement>(null);
 
   function toggleMenu() {
     setIsMenuOpen(!isMenuOpen);
   }
+
+  useEffect(() => {
+    if (!isMenuOpen) return;
+
+    function closeMenu(e: MouseEvent) {
+      // If the menu button is clicked, then the window event closes the menu
+      // and the menu button event opens it again, so we need to prevent that
+      // from happening.
+      if (menuButton.current?.contains(e.target as Node)) return;
+
+      setIsMenuOpen(false);
+    }
+
+    window.addEventListener("click", closeMenu, true);
+
+    return () => {
+      window.removeEventListener("click", closeMenu, true);
+    };
+  }, [isMenuOpen]);
 
   return (
     <nav className={styles.navbar}>
       <a href="#" className={styles.logo}>
         <Icon icon="logo" />
       </a>
-      <button onClick={toggleMenu} className={styles.menu}>
+      <button ref={menuButton} onClick={toggleMenu} className={styles.menu}>
         <Icon icon="menu" />
       </button>
       <ul
