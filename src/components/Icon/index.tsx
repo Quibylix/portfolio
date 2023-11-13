5cf6768fbdd98faffaf09aa46e6ecd7795d6f8ac
Vanaldito <barraza.fredimanuel@gmail.com>
2023-11-13 08:25:35 -0600

    feat: add new icons for HTML, CSS, NodeJS, and Javascript

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -1,11 +1,26 @@
 import ArrowDownIcon from "./ArrowDownIcon";
+import CSSIcon from "./CSSIcon";
 import GithubIcon from "./GithubIcon";
+import HTMLIcon from "./HTMLIcon";
+import JavascriptIcon from "./JavascriptIcon";
 import LogoIcon from "./LogoIcon";
 import MenuIcon from "./MenuIcon";
+import NodeJSIcon from "./NodeJSIcon";
+import ReactIcon from "./ReactIcon";
 import WebIcon from "./WebIcon";
 
 type IconProps = {
-  icon: "logo" | "menu" | "arrowDown" | "web" | "github";
+  icon:
+    | "logo"
+    | "menu"
+    | "arrowDown"
+    | "web"
+    | "github"
+    | "javascript"
+    | "html"
+    | "css"
+    | "nodeJS"
+    | "react";
 };
 
 export default function Icon({ icon }: IconProps) {
@@ -20,5 +35,15 @@ export default function Icon({ icon }: IconProps) {
       return <WebIcon />;
     case "github":
       return <GithubIcon />;
+    case "javascript":
+      return <JavascriptIcon />;
+    case "html":
+      return <HTMLIcon />;
+    case "css":
+      return <CSSIcon />;
+    case "nodeJS":
+      return <NodeJSIcon />;
+    case "react":
+      return <ReactIcon />;
   }
 }
