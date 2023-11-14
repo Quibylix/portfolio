31e380b3958dd2b655467a02681afe3ae16e5ba8
Vanaldito <barraza.fredimanuel@gmail.com>
2023-11-14 11:18:54 -0600

    feat: add form submission functionality to ContactMe component

--- src/components/ContactMe/index.tsx
+++ src/components/ContactMe/index.tsx
@@ -8,10 +8,16 @@ export default function ContactMe() {
       <p className={styles.description}>
         Connect with me and let's create something extraordinary together.
       </p>
-      <form className={styles.form}>
+      <form
+        action="https://formsubmit.co/4bccf8a6f9ed9d61fe7eca9875844bbf"
+        method="POST"
+        className={styles.form}
+      >
         <label className={styles.label}>
           <input
             className={styles.input}
+            required
+            pattern="^(?!\s*$).+"
             type="text"
             placeholder="Name"
             name="name"
@@ -23,6 +29,7 @@ export default function ContactMe() {
         <label className={styles.label}>
           <input
             className={styles.input}
+            pattern="[a-zA-Z0-9._%+\-]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,}"
             type="email"
             placeholder="Email"
             name="email"
@@ -32,7 +39,13 @@ export default function ContactMe() {
           </span>
         </label>
         <label className={styles.label}>
-          <textarea rows={3} className={styles.input} placeholder="Message" />
+          <textarea
+            rows={3}
+            required
+            className={styles.input}
+            placeholder="Message"
+            name="message"
+          />
           <span className={styles.icon}>
             <Icon icon="message" />
           </span>
